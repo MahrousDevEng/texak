5bf435f95b052a3f60a582dba128035f06539052
Mohamed Mahrous <eng.mahrous17@gmail.com>
2023-09-18 00:25:58 +0200

    add inner page

--- js/main.js
+++ js/main.js
@@ -1,38 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Banner Slider
-  const banner = document.getElementById("bannerSlider"),
-    bannerImages = Array.from(banner.querySelectorAll("img[data-src]"))?.map(
-      (item) => item.getAttribute("data-src")
-    );
-
-  if (banner) {
-    new Swiper(banner, {
-      loop: true,
-      speed: 2000,
-      parallax: true,
-      watchSlidesProgress: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: false,
-        waitForTransition: true,
-      },
-      preloadImages: false,
-      lazy: true,
-      navigation: {
-        nextEl: ".swiper-btn-next",
-        prevEl: ".swiper-btn-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return `<img src="${bannerImages[index]}" class="${className} smooth" />`;
-        },
-      },
-    });
-  }
-
   // Trigger LazyLoad
   const lazyLoadInstance = new LazyLoad({
     // Your custom settings go here
