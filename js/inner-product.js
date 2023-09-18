9f0a65e5cf2e5e54f3f20a8f3bec1cf89683e3cb
Mohamed Mahrous <eng.mahrous17@gmail.com>
2023-09-18 23:38:56 +0200

    add inner full

---
+++ js/inner-product.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  lightbox.option({
+    resizeDuration: 500,
+    fadeDuration: 500,
+    wrapAround: true,
+    positionFromTop: 100,
+  });
+});
